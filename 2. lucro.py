e62ece282286245ea9c31ed2a9f765902070da3a
joca <joaquimpaulovieirademeloo@gmail.com>
2025-04-15 11:25:40 -0300

    Update 2. lucro.py

--- 2. lucro.py
+++ 2. lucro.py
@@ -1,7 +1,12 @@
 valor_compra = float(input("Digite o valor de compra do produto: R$ ")) #Se é aplicando float em valor com (.) ou (,)
 
 def calcular_valor_venda(valor_compra):
-    if valor_compra < 10.00:
+    if valor de compra <= 0 #Verifica se o valor é valido
+    print(Digite um valor valido!)
+    exit()
+
+    #Verifica as condições para a aplicação das porcentagens
+    elif valor_compra < 10.00:  
         lucro = 1.70 
     elif 10.00 <= valor_compra < 30.00:
         lucro = 1.50  
@@ -12,8 +17,8 @@ def calcular_valor_venda(valor_compra):
     valor_venda = valor_compra * (lucro)
     return valor_venda
 
-valor_venda = calcular_valor_venda(valor_compra)
-valor_renda = (valor_venda) - (valor_compra)
+valor_venda = calcular_valor_venda(valor_compra) #Calcula o valor de venda após o lucro ser aplicado
+valor_renda = (valor_venda) - (valor_compra) #Calcula o lucro em comparação com valor antigo
 
-print(f"O valor de venda do produto é: R$ {valor_venda:.2f}")
+print(f"O valor de venda do produto é: R$ {valor_venda:.2f}") 
 print(f"tendo como renda o valor: R$ {valor_renda:.2f}")
